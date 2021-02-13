aec73eef12013e69eddbd82202f3e7845f132633
toni <ferreirocouto@gmail.com>
2021-02-13 21:52:49 +0100

    creacion de pagina about

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -6,8 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  public title: string;
+  public subtitle: string;
+  public web: string;
 
-  constructor() { }
+
+  constructor() { 
+    this.title = "Toni Ferreiro";
+    this.subtitle = "Ingeniero de software";
+    this.web= "https://toniferr.github.io";
+  }
 
   ngOnInit(): void {
   }
