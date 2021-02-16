4aa7a9c392a763dd11c0cde535b3d237b6550c47
toni <ferreirocouto@gmail.com>
2021-02-16 19:01:58 +0100

    listado de proyectos

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -21,7 +21,11 @@ export class ProjectService{
     saveProject(project: Project): Observable<any>{
         let params = JSON.stringify(project);
         let headers = new HttpHeaders().set('Content-Type', 'application/json');
-
         return this._http.post(this.url+'/save-project', params, {headers: headers});
+    }
+
+    getProjects(): Observable<any>{
+        let headers = new HttpHeaders().set('Content-Type', 'application/json');
+        return this._http.get(this.url+'/projects', {headers: headers});
     }
 }
